e99ae20418b5866aca151abac4d55498c50b42fc
Yasar Küskü <yasarkusku@hotmail.com>
2022-02-24 20:07:33 +0300

    commit on Program

--- Program.cs
+++ Program.cs
@@ -6,7 +6,46 @@ namespace HataYonetimi
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            try
+            {
+                System.Console.WriteLine("Bir Sayi Giriniz;");
+                int sayi = Convert.ToInt32(Console.ReadLine());
+                System.Console.WriteLine("Girmiş olduğunuz sayi:" + sayi);
+            }
+            catch(Exception ex)
+            {
+                System.Console.WriteLine("Hata:" + ex.Message.ToString());
+                
+            }
+            /*finally
+            {
+                System.Console.WriteLine("İşlme tamalandı.");
+            }*/
+
+            try
+            {
+                int a = int.Parse(null);
+            }
+            catch(ArgumentException ex)
+            {
+                System.Console.WriteLine("Boş değer girdiniz");
+                System.Console.WriteLine(ex);
+
+            }
+            catch(FormatException ex)
+            {
+                System.Console.WriteLine("Veri tipi uygun değil.");
+                System.Console.WriteLine(ex);
+            }
+            catch(OverflowException ex)
+            {
+                System.Console.WriteLine("Çok küçük yada çok büyül bi değer girdiniz.");
+                System.Console.WriteLine(ex);
+            }
+            finally
+            {
+                System.Console.WriteLine("İşlem başarıyla tamamlandı");
+            }
         }
     }
 }
